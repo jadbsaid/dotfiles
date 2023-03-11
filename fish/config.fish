2b15aac398ec13f49684162fa6ee9f5884b4c7ca
Jad Said <jadbsaid@gmail.com>
2023-03-11 15:57:04 -0400

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# go
+set -x GOPATH $HOME/go
+set -x GOROOT /usr/local/go
+set PATH $GOPATH/bin $GOROOT/bin $PATH
