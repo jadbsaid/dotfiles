2f7bb8f3f032d46ead890f0a6e21a6b7de33d121
Jad <jadbsaid@gmail.com>
2024-07-20 10:24:58 -0400

    fish: update py path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,8 @@ end
 
 # go
 fish_add_path (go env GOPATH)/bin
+
+# python
+set -xg PYTHONPATH /opt/homebrew/bin/python3 $PYTHONPATH
+alias python="/opt/homebrew/bin/python3"
+set -gx PATH /opt/homebrew/bin $PATH
