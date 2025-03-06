fcc2a78b2d64012d3cfc38b59fe26afcaec53771
Jad <jadbsaid@gmail.com>
2025-03-06 07:51:05 -0500

    update nvim key bindings

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,5 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- yank to clipboard
-vim.keymap.set("n", "<leader>y", "\" +y")
-vim.keymap.set("v", "<leader>y", "\" +y")
-vim.keymap.set("n", "<leader>Y", "\" +y")
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
