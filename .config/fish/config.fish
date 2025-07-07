550ad170c1f5799c33434a3b69e8c73bcdd037f9
Jad <jadbsaid@gmail.com>
2025-07-07 10:23:59 -0400

    ghostty: custom theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,15 @@ ulimit -n 31415
 # Local bin paths
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/go/bin
+
+# Theme settings, brighter syntax for Ghostty
+set -g fish_color_operator normal
+set -g fish_color_redirection normal
+set -g fish_color_comment normal
+set -g fish_color_valid_path normal
+set -g fish_color_cwd normal
+set -U fish_color_selection --background=#1f242a --foreground=#e6e1cf
 
 if status is-interactive
 end
