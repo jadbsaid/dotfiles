23948c1e9e402172f26570a0212d0f4ba6878737
Jad <jadbsaid@gmail.com>
2026-06-27 13:28:48 -0400

    nvim: more formatters

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,5 +1,5 @@
 return {
-    'stevearc/conform.nvim',
+    "stevearc/conform.nvim",
     config = function()
         require("conform").setup({
             formatters_by_ft = {
@@ -7,10 +7,20 @@ return {
                 cpp = { "clang-format" },
                 lua = { "stylua" },
                 go = { "gofmt" },
+                python = { "black" },
                 javascript = { "prettier" },
                 typescript = { "prettier" },
                 svelte = { "prettier" },
+                html = { "prettier" },
+                css = { "prettier" },
+                json = { "prettier" },
+                yaml = { "prettier" },
+                markdown = { "prettier" },
+            },
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_format = "fallback",
             },
         })
-    end
+    end,
 }
